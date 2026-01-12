6a25f78fef1ba84d2a637d2a70ff586746a4d003
yogesh09 <149040839+yogeshsuwarnkar@users.noreply.github.com>
2026-01-13 01:13:45 +0530

    Update and rename index.js to app.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
- 
-var app = express();//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
- res.send('changed not ');
-});//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
-module.exports = app;
